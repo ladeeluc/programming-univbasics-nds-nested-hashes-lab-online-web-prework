65f4a700c149e9e59ecf43f740157c7362c2e11f
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-22 03:22:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,12 +21,11 @@ end
 
 def literal_aoh
   more_people = [
-    { :name => "Don Gately", :occupation => "Live-in Staff" },
+       { :name => "Don Gately", :occupation => "Live-in Staff" }
        { :name => "Joelle van Dyne", :occupation => "Radio Personality" },
        { :name => "Pat Monteseian", :occupation => "Staff" },
        { :name  =>"Kate Gompert", :occupation => "None" },
        { :name =>  "Bruce Green", :occupation => "Fan of Mildred" }
-  
     ]
   
   
