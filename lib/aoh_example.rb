f8387f32ba3ddf6a372f47957be54705759203b8
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-22 03:08:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,11 +9,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh 
   
   people = [
-          DON_G, 
-          JOELLE_VD, 
-          PAT_M,
-          KATE_G,
-          BRUCE_G
+          
   ]
  
  
